70be78b29ddf138f64d9ca7bf893a17c37ad4ee6
Krishna Nuggihalli <Knuggihalli@sfsu.edu>
2023-03-06 23:55:24 -0800

    Continued work on the project, still needs work on the I/O but some bugs were fixed, and it will be ready to move to prod soon
    - Krishna Nuggihalli 3/6/2023

--- BikoProject.py
+++ BikoProject.py
@@ -11,15 +11,15 @@ import xlsxwriter
 #
 #
 
-# creates the final excel file
+# creates the final Excel file
 outputName = input("What do you want your file name to be?\n")
 seasonInput = input("Spring or Fall?\n")
 
 finalExcel = xlsxwriter.Workbook(outputName + ".xlsx")
 
 # creates a list with the worksheet names
-worksheets = ["contents" ,"key" ,"RAW SCORE DATA", "scoreADX", "scoreACX", "scoreARanks", "ScoreWDX",
- "scoreWCX", "scoreWRanks", "scoreASpeech", "rawDiffData", "diffACX", "diffWCX", "diffASpeech"]
+worksheets = ["contents", "key", "RAW SCORE DATA", "scoreADX", "scoreACX", "scoreARanks", "ScoreWDX",
+              "scoreWCX", "scoreWRanks", "scoreASpeech", "rawDiffData", "diffACX", "diffWCX", "diffASpeech"]
 
 # dynamically creates worksheet variables
 for sheet in worksheets:
@@ -28,22 +28,28 @@ for sheet in worksheets:
 # searches directory for Excel files parses them and then adds them to a dataset
 
 dataset = {}
+teamRoles = {}
 path = os.getcwd() + "\\Data"
+print("Loading Team Roles...")
 for file in os.listdir(path):
     if file.endswith(".xlsx") or file.endswith(".xlsm"):
-        dataset.update({os.path.splitext(file)[0]: pd.read_excel(path +"\\" + file, na_values="--", sheet_name=None, skiprows=[0])})
-path += "\\"+seasonInput
-print("Loading Excel Sheets...")
+        teamRoles = pd.read_excel(path + "\\" + file, na_values="--", sheet_name = seasonInput + " Roles")
+        print(file)
+path += "\\" + seasonInput
+print("Loading Tournament Sheets...")
 for tournamentFolder in os.listdir(path):
-    tmp = {}
-    for file in os.listdir(path+"\\"+tournamentFolder):
+    indivScores = {}
+    differences = {}
+    for file in os.listdir(path + "\\" + tournamentFolder):
         print(file)
         if file.endswith(".xlsx") or file.endswith(".xlsm"):
-            assert os.path.isfile(path + "\\"+tournamentFolder + "\\" + file)
-            tmp.update({os.path.splitext(file)[0]: pd.read_excel(path + "\\"+tournamentFolder + "\\" + file,
-                                                                 na_values="--", sheet_name=None,skiprows=[0])})
-    dataset.update({tournamentFolder : tmp})
-#for key in dataset.get('Black Lion - 2 REG').get('Preliminary Data').keys():
+            assert os.path.isfile(path + "\\" + tournamentFolder + "\\" + file)
+            differences.update({os.path.splitext(file)[0]: pd.read_excel(path + "\\" + tournamentFolder + "\\" + file,
+                                                            na_values="--", sheet_name="Differences", skiprows=[0])})
+            indivScores.update({os.path.splitext(file)[0]: pd.read_excel(path + "\\" + tournamentFolder + "\\" + file,
+                                                            na_values="--", sheet_name="Indiv Scores")})
+    dataset.update({tournamentFolder: [indivScores, differences]})
+# for key in dataset.get('Black Lion - 2 REG').get('Preliminary Data').keys():
 #   print(key + "\n")
 #   print(dataset.get('Black Lion - 2 REG').get('Preliminary Data').get(key))
 
@@ -57,50 +63,90 @@ worksheet.write('A2', 'World', bold)
 worksheet.write(2, 0, 123)
 worksheet.write(3, 0, 123.456)
 
-#worksheets[0].add_format('')
+
+# worksheets[0].add_format('')
 
 # class that contains values for each person
 class Person:
 
     def __str__(self):
-        return ": % s, % s, % s, % s, % s, % s, % s" % (
-            self.teamName, self.sideAsA, self.witnessDX, self.witnessCX,
-            self.sideAsW, self.witnessMain, self.witnessConting)
+        return ": % s, % s, % s, % s, % s, % s, % s, % s" % (
+            self.name, self.squad, self.roleOnP, self.roleOnD, self.witDX,
+            self.attyCX, self.attyDX, self.witConting)
 
     def __init__(self, this):
         # Datasheet
-        self.teamName = this.keys()[0]
-        self.name = this.keys()[1]
-        self.sideAsA = this.keys()[2]
-        self.witnessDX = this.keys()[3]
-        self.witnessCX = this.keys()[4]
-        self.sideAsW = this.keys()[5]
-        self.witnessMain = this.keys()[6]
-        self.witnessConting = this.keys()[7]
+        self.squad = this.get(this.keys()[0]).array[0]
+        self.name = this.get(this.keys()[1]).array[0]
+        self.roleOnP = this.get(this.keys()[2]).array[0]
+        self.roleOnD = this.get(this.keys()[3]).array[0]
+        self.attyDX = this.get(this.keys()[4]).array[0]
+        self.attyCX = this.get(this.keys()[5]).array[0]
+        self.witDX = this.get(this.keys()[6]).array[0]
+        self.witConting = this.get(this.keys()[7]).array[0]
 
         # Ballot Data
-        self
+
+        self.tournaments = {}
 
         # more properties
 
 pass
-seasonList = dataset.get('Team Roles')
-container = {}
-for season in seasonList:
-    tmp = []
-    counter = len(seasonList.get(season))
-    while counter:
-        tmpPerson = Person(pd.DataFrame(seasonList.get(season), index=[counter]))
-        print(pd.DataFrame(seasonList.get(season), index=[counter]).keys())
-        counter -= 1
-        tmp.insert(counter-1, {tmpPerson.name : tmpPerson})
-    container.update({season: tmp})
-# struct
-# container = {Spring{Person{general stuffs, {tournament 1}, {tournament 2}}, Person{}, Person{}}, Fall{Person{}, Person{}, Person{}}}
 
-print(container)
-#for season in container:
-    #for person in container.get(season):
-        #for a in person:
-            #print(a)
-finalExcel.close()
+# struct
+# People = {Person1{general stuffs, {tournament 1}, {tournament 2}}, Person2{}, Person3{}...}print("People: ")
+People = {}
+
+counter = len(teamRoles)
+
+while counter:
+    counter-=1
+    currPerson = Person(pd.DataFrame(teamRoles, index=[counter]))
+    # classic Del Gaudio workaround
+    People[currPerson.name.split(" ", 1)[1]] = currPerson
+i=0
+for folder in dataset:
+    for file in dataset.get(folder)[0]:
+        # Identification
+        currName = dataset.get(folder)[0].get(file).at[1,"Unnamed: %s" % 2]
+
+        # Indiv Scores
+
+        # Plaintiff
+        currDXP = dataset.get(folder)[0].get(file).at[2,"Unnamed: %s" % 3]
+        currCXP = dataset.get(folder)[0].get(file).at[2,"Unnamed: %s" % 4]
+        currSpeechP = dataset.get(folder)[0].get(file).at[2,"Unnamed: %s" % 5]
+        currRPP = dataset.get(folder)[0].get(file).at[2,"Unnamed: %s" % 6]
+
+        # Defense
+        currDXD = dataset.get(folder)[0].get(file).at[3,"Unnamed: %s" % 3]
+        currCXD = dataset.get(folder)[0].get(file).at[3, "Unnamed: %s" % 4]
+        currSpeechD = dataset.get(folder)[0].get(file).at[3, "Unnamed: %s" % 5]
+        currRPD = dataset.get(folder)[0].get(file).at[3, "Unnamed: %s" % 6]
+
+        # Differeneces
+        #print(dataset.get(folder)[1].get(file))
+        # Plaintiff
+        plaintiffCXRound1Judge1 = dataset.get(folder)[1].get(file).at[4,"Unnamed: %s" % 3]
+        plaintiffCXRound1Judge2 = dataset.get(folder)[1].get(file).at[5,"Unnamed: %s" % 3]
+        plaintiffCXRound2Judge1 = dataset.get(folder)[1].get(file).at[4,"Unnamed: %s" % 4]
+        plaintiffCXRound2Judge2 = dataset.get(folder)[1].get(file).at[5,"Unnamed: %s" % 4]
+        plaintiffSpeechRound1Judge1 = dataset.get(folder)[1].get(file).at[4,"Unnamed: %s" % 5]
+        plaintiffSpeechRound1Judge2 = dataset.get(folder)[1].get(file).at[5,"Unnamed: %s" % 5]
+        plaintiffSpeechRound2Judge1 = dataset.get(folder)[1].get(file).at[4,"Unnamed: %s" % 6]
+        plaintiffSpeechRound2Judge2 = dataset.get(folder)[1].get(file).at[5,"Unnamed: %s" % 6]
+
+        # Defense
+        defenseCXRound1Judge1 = dataset.get(folder)[1].get(file).at[6,"Unnamed: %s" % 3]
+        defenseCXRound1Judge2 = dataset.get(folder)[1].get(file).at[7,"Unnamed: %s" % 3]
+        defenseCXRound2Judge1 = dataset.get(folder)[1].get(file).at[6,"Unnamed: %s" % 4]
+        defenseCXRound2Judge2 = dataset.get(folder)[1].get(file).at[7,"Unnamed: %s" % 4]
+        defenseSpeechRound1Judge1 = dataset.get(folder)[1].get(file).at[6,"Unnamed: %s" % 5]
+        defenseSpeechRound1Judge2 = dataset.get(folder)[1].get(file).at[7,"Unnamed: %s" % 5]
+        defenseSpeechRound2Judge1 = dataset.get(folder)[1].get(file).at[6,"Unnamed: %s" % 6]
+        defenseSpeechRound2Judge2 = dataset.get(folder)[1].get(file).at[7,"Unnamed: %s" % 6]
+
+        print(defenseCXRound1Judge1)
+#print(People)
+
+#finalExcel.close()
